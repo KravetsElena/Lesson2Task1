7016eb37854428f475dd037993b204e24f0a0a15
ElenaKr <kravets_2024@internet.ru>
2023-04-13 09:38:14 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 12365;
+        int oneBonusMile = 20;
+
+        int bonus = price / oneBonusMile;
+
+        System.out.println(bonus);
+    }
+}
